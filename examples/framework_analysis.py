3bc608e144ecd4c99a15f61621433fc30a1b43ad
Jonathan <jonathan@forestandrock.co.uk>
2025-03-07 08:50:49 +0100

    added comments and docstrings

--- examples/framework_analysis.py
+++ examples/framework_analysis.py
@@ -3,40 +3,52 @@ import math as m
 import random
 
 from itertools import chain
+from typing import Dict
 
 import matplotlib.pyplot as plt
 
 from sentinelpricing import Framework, LookupTable, TestSuite, TestCase
 
 
-def generate_random_testcase():
+def generate_random_testcase() -> Dict[str, float]:
+	""" Generate Random Testcase 
+	
+	Creates a dictionary of keys "age" and "lic" with random values.
+	"""
+
 	return {
 		"age": round(random.gammavariate(5, 45 / 5)),
 		"lic": 9 - round(random.expovariate(0.8))
 	}
 
 
+# Creates a list of dictionaries, compatible with the lookup table
+# These are the current rates we want to analyse
 age_rates = [
 	{"age": a, "rate": 1.5 * max((17/a),0.8)}
 	for a in chain([17,], range(20, 80, 15))
 ]
 
+# Creates a list of dictionaries, compatible with the lookup table
+# These are the current rates we want to analyse
 lic_rates = [
 	{"lic": l, "rate": max(1 - (l * 0.2), 0.8)}
 	for l in range(0,8)
 ]
 
+# Creates a list of dictionaries, compatible with the lookup table
+# These are the proposed rates we want to analyse.
 proposed_age_rates = [
 	{"age": a, "rate": 1.4 * max((17/a),0.75)}
 	for a in chain([17,], range(20, 80, 1))
 ]
 
 
+# Here we create the random testcases, in reality though you would likely be
+# loading these from a csv file or similar.
 testcases = TestSuite(
 	[
-		TestCase(
-			generate_random_testcase()
-		)
+		generate_random_testcase()
 		for _ in range(10_000)
 	]
 )
