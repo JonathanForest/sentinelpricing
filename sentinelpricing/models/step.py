ca0eca6262813595905f126d9bc5ca5ecba61a99
Jonathan <jonathan@forestandrock.co.uk>
2025-03-08 23:53:27 +0100

    rounded final price, and extended name in repr

--- sentinelpricing/models/step.py
+++ sentinelpricing/models/step.py
@@ -12,6 +12,11 @@ class Step:
         self.other = other
         self.result = result
 
+    @classmethod
+    def headers(cls):
+        return f"{'Total': <8} :: {"Name":<72}" + \
+        f" - {"Operation": <30} - {"Other"}"
+
     def __repr__(self):
-        return f"{self.result: <7} :: {self.name:<30}"
+        return f"{round(self.result, 5): <9} :: {self.name:<72}" + \
         f" - {repr(self.oper): <30} - {self.other}"
